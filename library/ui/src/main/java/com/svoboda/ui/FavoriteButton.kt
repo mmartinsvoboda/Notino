eed222e02ef22c40b92c8d36abd6d45082a46954
Martin Svoboda <martin.svoboda@etnetera.eu>
2023-04-12 21:29:39 +0200

    Fixed up FavoriteButton.kt

--- library/ui/src/main/java/com/svoboda/ui/FavoriteButton.kt
+++ library/ui/src/main/java/com/svoboda/ui/FavoriteButton.kt
@@ -1,13 +1,18 @@
 package com.svoboda.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.StarOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FavoriteButton(
@@ -21,9 +26,11 @@ fun FavoriteButton(
         onClick = onClick,
         modifier = modifier
     ) {
-        if (isFavorite)
-            Icon(Icons.Default.Favorite, null, tint = favoriteColor)
-        else
-            Icon(Icons.Outlined.FavoriteBorder, null, tint = defaultColor)
+        Icon(
+            imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Outlined.FavoriteBorder,
+            contentDescription = null,
+            modifier = Modifier.size(16.dp),
+            tint = if (isFavorite) favoriteColor else defaultColor
+        )
     }
 }
