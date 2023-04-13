c2ffce04f6cb17d10dc276a59a8a375a30ff1ce9
Martin Svoboda <martin.svoboda@etnetera.eu>
2023-04-13 15:20:41 +0200

    KDoc

--- library/local/src/main/java/com/svoboda/database/domain/repository/FavoriteProductRepository.kt
+++ library/local/src/main/java/com/svoboda/database/domain/repository/FavoriteProductRepository.kt
@@ -2,18 +2,39 @@ package com.svoboda.database.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Repository interface for managing favorite products, providing methods for observing, adding, and removing favorite products.
+ */
 interface FavoriteProductRepository {
 
-    // Observes all favorite products
+    /**
+     * Observes a list of IDs of all favorite products.
+     *
+     * @return A [Flow] containing a list of favorite product IDs.
+     */
     fun observeAllFavoriteProducts(): Flow<List<Int>>
 
-    // Observes favorite product by id
+    /**
+     * Observes a specific favorite product by its ID.
+     *
+     * @param productId The ID of the product to observe.
+     * @return A [Flow] containing the favorite product ID if it exists, or `null` if it doesn't.
+     */
     fun observeFavoriteProducts(productId: Int): Flow<Int?>
 
-    // Adds product to favorites
+    /**
+     * Adds a product to the list of favorites.
+     *
+     * @param productId The ID of the product to add to the favorites.
+     */
     suspend fun setFavoriteProduct(productId: Int)
 
-    // Removes product from favorites
+    /**
+     * Removes a product from the list of favorites.
+     *
+     * @param productId The ID of the product to remove from the favorites.
+     */
     suspend fun deleteFavoriteProduct(productId: Int)
 
 }
+
